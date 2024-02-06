e2783aebb5bed02aa7d97781a24220f7df8a3de1
yerincho06 <yerincho06@gmail.com>
2024-02-06 11:54:02 +0900

    servlet

---
+++ chap01-life-cycle-lecture-source/src/main/java/com/ohgiraffers/section01/xml/LifeCycleTestServlet.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.section01.xml;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+public class LifeCycleTestServlet extends HttpServlet {
+
+    public LifeCycleTestServlet(){
+        System.out.println("xml 방식 기본 생성자 실행!");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("xml 매핑 init() 메소드 호출");
+    }
+
+    @Override
+    public void destroy(){
+        System.out.println("xml 매핑 destroy() 메소드 호출");
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+    }
+}
